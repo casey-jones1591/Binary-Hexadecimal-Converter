3ffd717d38d479c11517b2b6ccc0637437e31001
Casey <47370829+casey-jones1591@users.noreply.github.com>
2019-02-12 21:37:29 -0600

    Update solution1.c

--- solution1.c
+++ solution1.c
@@ -1,65 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include  <sys/types.h>
-
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char* argv[]){
 
-	FILE* fp;
-	char line[1024];
-	int i=0;	
+	FILE* cmd_File;
+	char cmd_line[1024];	
 
 	/* error check that a file is given */
 	if (argc != 2){
-		printf("An error has occured\n");
+		fprintf(stderr, "An error has occured\n");
 		exit(1);
 	}
 	
 	/* open file */
-	fp=fopen(argv[1], "r" );
+	cmd_File=fopen(argv[1], "r" );
 	
 	/* loop through each line */
-	while(fgets(line, sizeof(line), fp) != NULL){
+	while(fgets(cmd_line, sizeof(cmd_line), cmd_File) != NULL){
 		char *myargs[4]={ NULL }; 
 		char *token;
-		int idx;
 		int count = 0;
 
 		/* tokenize each line */
-		token = strtok(line, " \t\n");
+		token = strtok(cmd_line, " \t\n");
 		while (token && (count < 4)){
 			myargs[count++] = strdup(token);
 			token = strtok(NULL, " \t\n");
 		}
 
 		/* call fork and make system call */
+		/* credit: fork() call & processes are based off our text book OSTEP */
 		int rc = fork();
+
 		/* fork faiiled */
 		if (rc < 0) {  			
-			printf("An error has occured\n");
+			fprintf(stderr,"An error has occured\n");
 	       		exit(1);
 
 		/* child new process */
 		} else if (rc == 0) {  		
-	   		printf("hello, I am child (pid:%d)\n", (int) getpid());
 			execvp(myargs[0], myargs); 
-			printf("this shouldn't print out\n");
-
-		/* parent new process */
+			
+		/* parent process & wait until child process is completed */
 	   	} else { 
 			int wc = wait(NULL);
-	       		printf("hello, I am parent of %d (wc:%d) (pid:%d)\n",
-		       		rc, wc, (int) getpid());
+
 	    	}
 
 		/* loop through myargs to deallocate */
-		for (idx = 0; idx < count; idx++) {
+		for (int idx = 0; idx < count; idx++) {
 			free(myargs[idx]);
 		}
 			
 	}
 	/* close file */
-	fclose(fp);
+	fclose(cmd_File);
 	return 0;
 }
